ad5752e38479d506da2640c3347e86055a3c8f34
equintanilla20 <35775664+equintanilla20@users.noreply.github.com>
2022-09-16 22:22:18 -0500

    Begin hw1 problem set

---
+++ src/hw1/HomeworkOne.java
@@ -0,0 +1,39 @@
+package hw1;
+
+public class HomeworkOne {
+    // 1) Write a Java method that will take a string and reverse it. Can you come up with a method that is faster
+    // than regular reversing of the for loop?
+    public String strReverse(String input) {
+        return "answer";
+    }
+
+    // 2) Write a Java program that generates a new string by concatenating the reversed substrings of even
+    // indexes and odd indexes separately from a given string. For this code you may use the above method to
+    // reverse the string.
+    public String strGenerator(String input) {
+        return "answer";
+    }
+
+    // 3) In a bag there are N distinct coins. Each coin has a value between 1 and N. However, there is one coin
+    // that is marked X. How will you find the value of X in an efficient way?
+    public int coinValue (int n) {
+        return 0;
+    }
+
+    // 4) Generate two random arrays of size n > 100 from a set S= {‘A’, ‘C’, ‘T’, ’G’}. Return the first index if you
+    // can find only 4 consecutive “G”s in both the arrays at the same indexes. Experiment with different values
+    // of n.
+    public int questionFour(char[] input) {
+        return 0;
+    }
+
+    // 5) Write a Java method that will take an array of integers of size n and shift right by m places, where n >
+    // m. You can read your inputs from a file and write your outputs to another file.
+    public void questionFive(int[] input) {
+        // output to file
+    }
+
+    public static void main(String[] args) {
+        HomeworkOne hw1 = new HomeworkOne();
+    }
+}
--- src/test/TestClass.java
+++ src/test/TestClass.java
@@ -10,8 +10,8 @@ public class TestClass {
         JButton b = new JButton("Click");
         b.setBounds(130,100,100, 40);
         jframe.add(b);
-        jframe.setSize(400,500); //400 width and 500 height
-        jframe.setLayout(null); //using no layout managers
-        jframe.setVisible(true); //making the frame visible
+        jframe.setSize(400,500); // 400 width and 500 height
+        jframe.setLayout(null);             // using no layout managers
+        jframe.setVisible(true);            // making the frame visible
     }
 }
