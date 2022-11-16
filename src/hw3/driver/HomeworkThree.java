2f6aa011e650449116360145f833840e2f2dc64c
Ezekiel Quintanilla <equintanilla20@gmail.com>
2022-11-15 20:56:49 -0600

    Implementing HW3Q1

--- src/hw3/driver/HomeworkThree.java
+++ src/hw3/driver/HomeworkThree.java
@@ -1,8 +1,12 @@
 package hw3.driver;
+import hw3.q1.QuestionOne;
 
 public class HomeworkThree {
 
     public static void main(String[] args) {
         System.out.println("Begin HW3");
+        QuestionOne q1 = new QuestionOne();
+        q1.analyzeFormula(1, 2, 3);
+
     }
 }
--- src/hw3/q1/QuestionOne.java
+++ src/hw3/q1/QuestionOne.java
@@ -1,4 +1,20 @@
 package hw3.q1;
 
 public class QuestionOne {
+    /*
+    1. Quadratic Equation
+    Design and write a Java code that solves a quadratic equation ax^2 + bx + c = 0. Please use class and
+    object definitions and do not use procedural style.
+    Let the roots of ax^2 + bx + c = 0 be p and q. Show that the following:
+            p + q = -b/a and pq = c/a
+    for non-trivial a, b, c. Make sure that you address that complex numbers as objects.
+    */
+    public void analyzeFormula(int a, int b, int c) {
+        Formula formula = new Formula(a, b, c);
+        formula.findPQ();
+        formula.showBOverA();
+        formula.showPPlusQ();
+        formula.showCOverA();
+        formula.showPTimesQ();
+    }
 }
