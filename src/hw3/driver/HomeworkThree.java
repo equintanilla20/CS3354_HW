8d945c2beb15d671c2e488d6ccbc6c73f2da8f2a
Ezekiel Quintanilla <equintanilla20@gmail.com>
2022-11-24 22:35:58 -0600

    Implementing HW3Q4

--- src/hw3/driver/HomeworkThree.java
+++ src/hw3/driver/HomeworkThree.java
@@ -2,6 +2,7 @@ package hw3.driver;
 import hw3.q1.QuestionOne;
 import hw3.q2.QuestionTwo;
 import hw3.q3.QuestionThree;
+import hw3.q4.QuestionFour;
 
 public class HomeworkThree {
 
@@ -10,9 +11,11 @@ public class HomeworkThree {
         QuestionOne q1 = new QuestionOne();
         QuestionTwo q2 = new QuestionTwo();
         QuestionThree q3 = new QuestionThree();
-        q1.analyzeFormula(1, 2, 0);
-        System.out.println("Q2: " + q2.isValid("abcdefghhgfedecba"));
-        System.out.println("Q3: " + q3.commonSubString("and", "art"));
+        QuestionFour q4 = new QuestionFour();
 
+//        q1.analyzeFormula(1, 2, 0);
+//        System.out.println("Q2: " + q2.isValid("abcdefghhgfedecba"));
+//        System.out.println("Q3: " + q3.commonSubString("and", "art"));
+        q4.simulate();
     }
 }
--- src/hw3/q4/QuestionFour.java
+++ src/hw3/q4/QuestionFour.java
@@ -4,32 +4,77 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Random;
 
-public class QuestionFour {
+public class QuestionFour extends Thread {
     /*
     4. Simulate 3-Queue System
     */
     private Random rand = new Random();
+    private char type;
+    private int finished;
     Queue<Person> queueA = new LinkedList<>();
     Queue<Person> queueB = new LinkedList<>();
     Queue<Person> queueC = new LinkedList<>();
+
+    public QuestionFour() {
+        type = ' ';
+    }
+
+    QuestionFour(char type) {
+        this.type = type;
+    }
+
     private Person arrival() {
         int waitTime = rand.nextInt(15) + 1;
         Person person = new Person(waitTime);
         return person;
     }
 
-    public void simulate() {
-        Thread tsaCheckerA = new TSA(queueA, queueC);
-        Thread tsaCheckerB = new TSA(queueB, queueC);
-        Thread tsaCheckerC = new TSA(queueC);
-
-        while (true) {
-            if (rand.nextInt(2) == 1) {
-
+    public void run() {
+        if (type == 'A') {
+            while (queueA.isEmpty()) {}
+            try {
+                System.out.println("A: " + queueA.peek().getWaitTime());
+                wait(queueA.peek().getWaitTime());
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
             }
-            tsaCheckerA.start();
-            tsaCheckerB.start();
-            tsaCheckerC.start();
+            queueC.add(queueA.remove());
+        } else if (type == 'B') {
+            while (queueB.isEmpty()) {}
+            try {
+                System.out.println("B: " + queueB.peek().getWaitTime());
+                wait(queueB.peek().getWaitTime());
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            queueC.add(queueB.remove());
+        } else {
+            while (queueC.isEmpty()) {}
+            try {
+                System.out.println("C: " + queueC.peek().getWaitTime());
+                wait(queueC.peek().getWaitTime());
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            finished++;
+            queueC.remove();
+        }
+    }
+
+    public void simulate() {
+        QuestionFour checkerA = new QuestionFour('A');
+        QuestionFour checkerB = new QuestionFour('B');
+        QuestionFour checkerC = new QuestionFour('C');
+        queueA.add(arrival());
+        checkerA.start();
+        checkerB.start();
+        checkerC.start();
+        while (finished < 50) {
+            if (rand.nextInt(2) == 1) {
+                queueA.add(arrival());
+            } else {
+                queueB.add(arrival());
+            }
         }
     }
 
